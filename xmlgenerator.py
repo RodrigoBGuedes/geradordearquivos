f5276e236665c8890403f277dcd2f00cd706f116
rodrigo.guedes <gedsao77@hotmail.com>
2022-10-04 15:33:46 -0300

    Gerado o arquivo que cria arquivos xml em um diretório

---
+++ xmlgenerator.py
@@ -0,0 +1,40 @@
+import os
+import time
+import argparse
+import random
+from datetime import datetime
+from pathlib import Path
+
+parser = argparse.ArgumentParser(description="Generate random XML file name in specific time")
+parser.add_argument("-t", "--time", default=3)
+option = parser.parse_args()
+
+time_between_creation = int(option.time)
+
+
+def generatexml(filename):
+    folder = Path('xmls')
+    if not os.path.exists(folder):
+        Path("xmls").mkdir(parents=True, exist_ok=True)
+
+    now = datetime.now()
+
+    sample_names_values = ("Auditorio", 'Marcenaria', 'Saguao', 'Salao', 'Sala-de-estar')
+    sample_material = random.randrange(1, 9999)
+    sample_name = random.choice(sample_names_values)
+    sample_date = now.strftime('%Y%m%d_%H_%M_%S')
+
+    txt_filename = str(sample_name) + '_' + str(sample_material) + '_' + str(sample_date) + ".xml"
+    new_txt = Path(folder, str(txt_filename))
+
+    f = open(new_txt, "a")
+    f.writelines(str(sample_name) + filename)
+    f.close()
+
+    print(f"Writing log file: {new_txt}\n")
+    return
+
+
+while 1:
+    generatexml(f'{random.randrange(50, 350)}')
+    time.sleep(time_between_creation)
