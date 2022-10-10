f684bcfd55a278e2ed97184b322aa8305747312a
rodrigo.guedes <gedsao77@hotmail.com>
2022-10-10 16:26:37 -0300

    Revertendo para o modelo certo

--- xmlgenerator.py
+++ xmlgenerator.py
@@ -7,46 +7,54 @@ from pathlib import Path
 import xml.etree.ElementTree as gfg
 
 parser = argparse.ArgumentParser(description="Generate random XML file name in specific time")
-parser.add_argument("-t", "--time", default=3)
-option = parser.parse_args()
+parser.add_argument("-q", "--quantidade", required=True)
+parser.add_argument("-t", "--time", default=5)
+options = parser.parse_args()
 
-time_between_creation = int(option.time)
+time_between_creation = int(options.time)
+quantidade_criada = int(options.quantidade)
+count = 0
 
 
-def generatexml(filename):
+def generatexml(quantidade_criada):
+
     folder = Path('xmls')
+
     if not os.path.exists(folder):
         Path("xmls").mkdir(parents=True, exist_ok=True)
 
-    now = datetime.now()
+    for _ in range(quantidade_criada):
+        now = datetime.now()
 
-    sample_names_values = ("Auditorio", 'Marcenaria', 'Saguao', 'Salao', 'Sala-de-estar')
-    sample_material = random.randrange(1, 9999)
-    sample_name = random.choice(sample_names_values)
-    sample_date = now.strftime('%Y-%m-%d_%Hh-%Mm-%Ss')
+        sample_names_values = ("Auditorio", 'Marcenaria', 'Saguao', 'Salao', 'Sala-de-estar')
+        sample_material = random.randrange(1, 9999)
+        sample_name = random.choice(sample_names_values)
+        sample_date = now.strftime('%Y-%m-%d_%Hh-%Mm-%Ss')
 
-    txt_filename = str(sample_name) + '_' + str(sample_material) + '_' + str(sample_date) + ".xml"
-    new_txt = Path(folder, str(txt_filename))
+        txt_filename = str(sample_name) + '_' + str(sample_material) + '_' + str(sample_date) + ".xml"
+        new_txt = Path(folder, str(txt_filename))
 
-    root = gfg.Element("Tipos")
+        root = gfg.Element("Tipos")
 
-    m1 = gfg.Element("Comodos")
-    root.append(m1)
+        m1 = gfg.Element("Comodos")
+        root.append(m1)
 
-    b1 = gfg.SubElement(m1, "name")
-    b1.text = str(sample_name)
-    b2 = gfg.SubElement(m1, "number")
-    b2.text = str(sample_material)
+        b1 = gfg.SubElement(m1, "name")
+        b1.text = str(sample_name)
+        b2 = gfg.SubElement(m1, "number")
+        b2.text = str(sample_material)
 
-    tree = gfg.ElementTree(root)
+        tree = gfg.ElementTree(root)
 
-    with open(new_txt, "wb") as files:
-        tree.write(files)
+        with open(new_txt, "wb") as files:
+            tree.write(files)
 
-    print(f"Writing log file: {new_txt}\n")
+        print(f"Writing log file: {new_txt}\n")
     return
 
 
 while 1:
-    generatexml('')
+    generatexml(quantidade_criada)
+    count += quantidade_criada
+    print(f'Foram gerados:{count} arquivos XML')
     time.sleep(time_between_creation)
